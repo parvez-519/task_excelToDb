92f94ac09669ef2dbc725ec5dcd0a854eded8cbe
Parvez Pathan <parvez.pathan.hyd@xelpmoc.in>
2022-12-08 21:19:12 +0530

    modifications

--- api/controllers/emp/emp.js
+++ api/controllers/emp/emp.js
@@ -15,10 +15,11 @@ const uploadData = async (req, res) => {
     let notString = false;
     let notNumber = false;
     let diffColLength = false;
-
-    let rowsNum = 1;
+    let isNull = false;
+    let rowsNum = 0;
     let cellNum = 1;
     var alphabets = /^[A-Za-z]*$/;
+
     readXlsxFile(path).then((rows) => {
       // skip header
       rows.shift();
@@ -27,13 +28,13 @@ const uploadData = async (req, res) => {
 
         let arr = {
           name: row[0],
-          age: row[1]
+          age: row[1],
         };
-
         var size = Object.keys(arr).length;
-        if(size!=row.length)
-        {
-          diffColLength=true
+        // HEADERS VALIDATION FOR SAME COLUMN SIZE
+        if (size != row.length) {
+          console.log("------inside size---------");
+          diffColLength = true;
           res.status(400).json({
             status: appConst.status.fail,
             response: null,
@@ -41,43 +42,68 @@ const uploadData = async (req, res) => {
           });
           break;
         }
-        else if (!row[0].match(alphabets)) {
+        // CHECKING FOR ANY EMPTY CELL
+        if (row[0] === null) {
+          console.log("------inside row---------");
+          isNull = true;
+          res.status(400).json({
+            status: appConst.status.fail,
+            response: null,
+            message: `Failed ! empty cell at row ${rowsNum} and column 1`,
+          });
+          console.log("------inside row 222---------");
+
+          break;
+        }
+        // IF NO EMPTY CELL THEN PERFORM REGEX OPERATION FOR STRING VALIDATION ON NAME COLUMN
+         else if (!row[0].match(alphabets)) {
+          console.log("------inside row else---------");
+
           notString = true;
           res.status(400).json({
             status: appConst.status.fail,
             response: null,
-            message: `Failed ! Not string at row ${rowsNum / 2} column 1`,
+            message: `Failed ! Not string at row ${rowsNum} column 1`,
           });
           break;
         }
-        else if (typeof row[1] != "number") {
+        // CHECKING FOR NUMBER VALIDATION ON AGE COLUMN
+        if (typeof row[1] != "number" && isNull === false) {
+          console.log("------inside typeof ---------");
+
           notNumber = true;
           res.status(400).json({
             status: appConst.status.fail,
             response: null,
-            message: `Failed ! Not number at row ${rowsNum / 2} column 2`,
+            message: `Failed ! Not number at row ${rowsNum} column 2`,
           });
           break;
         }
-        rowsNum++;
-        if (notString != true && notNumber != true && diffColLength!=true) {
-          for (let cell of row) {
-            if (cell === null) {
-              cellNum++;
-              isEmpty = true;
-              res.status(400).json({
-                status: appConst.status.fail,
-                response: { rowNum: rowsNum, cellNum: cellNum },
-                message: "Failed ! All rows required",
-              });
-            }
-          }
-        }
+        
+        // if (notString != true && notNumber != true && diffColLength!=true) {
+        //   for (let cell of row) {
+        //     if (cell === null) {
+        //       cellNum++;
+        //       isEmpty = true;
+        //       res.status(400).json({
+        //         status: appConst.status.fail,
+        //         response: { rowNum: rowsNum, cellNum: cellNum },
+        //         message: "Failed ! All rows required",
+        //       });
+        //     }
+        //   }
+        // }
 
         arrayData.push(arr);
       }
-
-      if (isEmpty === false && notString != true && notNumber != true && diffColLength!=true) {
+      // AFTER CHECKING ABOVE ALL SCENERIO SAVE THE DATA TO DB
+      if (
+        isEmpty === false &&
+        notString != true &&
+        notNumber != true &&
+        diffColLength != true &&
+        isNull === false
+      ) {
         empRepo.bulkCreate(arrayData);
         res.status(200).json({
           status: appConst.status.success,
